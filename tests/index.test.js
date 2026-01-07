4ba6fec9d97d1e7a5e55b07895e78639c1bc4fb6
Jesus <jesuscolladotocon@gmail.com>
2026-01-07 12:20:57 +0100

    Restaurado HTML y test para HITO 4

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,6 +1,4 @@
-const fs = require('fs');
-
-test('index.html existe', () => {
-  expect(fs.existsSync('index.html')).toBe(true);
+test('test de fallo intencionado', () => {
+  expect(true).toBe(false);
 });
 
